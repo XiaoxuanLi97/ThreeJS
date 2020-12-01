bd4a89746484d04a6acc05de3d0ec43880ef619b
lixiaoxuan <ucesikx@ucl.ac.uk>
2020-12-01 16:29:05 +0800

    增加场景切换功能

--- forgeVue/src/components/forge.vue
+++ forgeVue/src/components/forge.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="forgeViewer"></div>
+  <div id="forgeViewer">
+    <div id="select">
+      <label for="sceneSelect">选择场景 </label>
+      <select id="sceneSelect" @change="changeScene">
+      <option v-for="item in optionList" :value="item.id">场景{{item.id}}</option>
+      </select>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,18 +14,21 @@ export default {
   name: "forge",
   data(){
     return{
-      viewer:[],
+      viewer:null,
       optionList:[
        {
-           env:'Local',
-           model_src: "http://bim.ndwp.net:8876/forgescene/liuzaojiangzhazhan_20190905/3d.svf"
+         id:'1',
+         env:'Local',
+         model_src: "http://bim.ndwp.net:8876/forgescene/liuzaojiangzhazhan_20190905/3d.svf"
        },
        {
+         id:'2',
          env: "Local",
          model_src:
              "http://bim.ndwp.net:8876/forgescene/wuzaojiangzha_1/3d.svf"
        },
        {
+         id:'3',
          env: "Local",
          model_src:
              "http://bim.ndwp.net:8876/forgescene/kongpuzhazhan/svf/3d.svf"
@@ -38,17 +48,27 @@ export default {
         }
         console.log('Initialization complete, loading a model next...');
       });
-
-
-      this.viewer = new Autodesk.Viewing.Private.GuiViewer3D(document.getElementById('forgeViewer'),{});
+      this.load(options)
+    },
+    load(options) {
+      this.viewer = new Autodesk.Viewing.Private.GuiViewer3D(document.getElementById('forgeViewer'), {});
       Autodesk.Viewing.Initializer(options, () => {
         this.viewer.initialize(); //创建DOM和canvas元素，设置WebGL
         this.viewer.loadModel(options.model_src); //加载模型到查看器中
       })
+    },
+
+    changeScene(){
+      let e = document.getElementById('sceneSelect').value
+      this.optionList.forEach(value => {
+        if (value.id === e){
+          this.load(value)
+        }
+      })
     }
   },
   mounted(){
-    this.init(this.optionList[0]);
+    this.init(this.optionList[0])
   }
 }
 </script>
@@ -62,5 +82,19 @@ body{
   height: 100%;
   margin: 0;
   backgroud-color:#F0F8FF;
+}
+
+#select {
+  position: absolute;
+  z-index: 2;
+  right: 20px;
+  top: 20px;
+  color: #f4f4f4;
+  background-color: rgba(34, 34, 34, 0.94);
+  box-shadow: 1px 3px 10px 0 rgba(0, 0, 0, 0.3);
+  outline: none;
+  border-radius: 3px;
+  font-weight: bold;
+  font-family:Verdana,serif;
 }
 </style>
